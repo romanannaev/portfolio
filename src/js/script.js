8353e96c23144d06a8f2d3905cc7bf2581b4ecdf
Raman Annayeu <r.annayeu@gmail.com>
2024-03-03 23:47:30 +0300

    fourth screen

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,17 @@ function toggleMenuActive (triggerObject) {
 
 toggleMenuActive(hamburger);
 toggleMenuActive(menu);
+
+
+const progressItems = document.querySelectorAll('.progress__item');
+progressItems.forEach((item) => {
+    const progressValue = item.querySelector('.progress__item-value');
+    const progressSpan = item.querySelector('.progress__item-chart span');
+
+    if (progressValue && progressSpan) {
+        // TODO add some checkings(% etc.)
+        progressSpan.style.width = progressValue.textContent;
+    } else {
+        console.log("Something went wrong...")
+    }
+});
